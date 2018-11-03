4f9450b52454ae4fd6c96d1894b9e346f44e021c
yanyuzm <xzm_28@163.com>
2018-11-03 21:45:01 +0800

    add file

---
+++ aliyun_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#这是我在阿里云上的镜像，v1.12.0版本
+#执行此脚本之后，就可以使用kubeadm初始化集群了
+
+images=(kube-apiserver:v1.12.0 kube-controller-manager:v1.12.0 kube-scheduler:v1.12.0 kube-proxy:v1.12.0 pause:3.1 etcd:3.2.24 coredns:1.2.2)
+
+for ima in ${images[@]}
+do
+   docker pull   registry.cn-shenzhen.aliyuncs.com/lurenjia/$ima
+   docker tag    registry.cn-shenzhen.aliyuncs.com/lurenjia/$ima   k8s.gcr.io/$ima
+   docker rmi  -f $(docker images |grep registry.cn-shenzhen.aliyuncs.com |awk '{print $1":"$2}')
+
+done
