3ceaa58dc382355d23fcf065592376fd5c2a65cb
yanyuzm <xzm_28@163.com>
2018-11-03 22:34:20 +0800

    add file

--- dockerhub_image.sh
+++ dockerhub_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #这是dockerhub上的镜像，v1.12.2版本
 
-images=(kube-apiserver-amd64:v1.12.2 kube-controller-manager-amd64:v1.12.2 kube-scheduler-amd64:v1.12.2 kube-proxy-amd64:v1.12.2 pause-amd64:3.1 etcd-amd64:3.2.24 coredns-amd64:1.2.2)
+images=(kube-apiserver:v1.12.2 kube-controller-manager:v1.12.2 kube-scheduler:v1.12.2 kube-proxy:v1.12.2 pause:3.1 etcd:3.2.24 coredns:1.2.2)
 images2=flannel:v0.10.0-amd64
 for ima in ${images[@]}
 do
