b26635e27bcb1fb22b68ab89c95baf3f1da9b919
yanyuzm <xzm_28@163.com>
2018-11-04 16:18:43 +0800

    add file

--- aliyun_image.sh
+++ aliyun_image.sh
@@ -2,7 +2,7 @@
 #这是我在阿里云上的镜像，v1.12.0版本
 #执行此脚本之后，就可以使用kubeadm初始化集群了
 
-images=(kube-apiserver:v1.12.0 kube-controller-manager:v1.12.0 kube-scheduler:v1.12.0 kube-proxy:v1.12.0 pause:3.1 etcd:3.2.24 coredns:1.2.2)
+images=(kube-apiserver:v1.12.0 kube-controller-manager:v1.12.0 kube-scheduler:v1.12.0 kube-proxy:v1.12.0 kubernetes-dashboard-amd64:v1.10.0 pause:3.1 etcd:3.2.24 coredns:1.2.2)
 
 for ima in ${images[@]}
 do
